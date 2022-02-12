2610990ba7c44281154d258e8236c98af5061381
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-02-12 11:04:43 -0300

    instalando nodemon e configurando o nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-  response.json({ message: 'Hello World 🚀🚀🚀' })
+  response.json({
+    message: 'Hello World - Ignite NodeJs Partiu decolar 🚀🚀🚀'
+  })
 })
 
 app.listen(3333, () => console.log(` RUNNING PORT 3333 🚀🚀🚀 `))
