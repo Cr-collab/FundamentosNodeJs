51cccc5791ca981c9583d424244d69b403b8d615
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-02-12 10:47:03 -0300

    criando nosso package alem de iniciar com express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World 🚀🚀🚀' })
+})
+
+app.listen(3333, () => console.log(` RUNNING PORT 3333 🚀🚀🚀 `))
